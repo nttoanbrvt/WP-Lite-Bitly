4f375ad541d40055606b658d0252b390d3902455
Toan Nguyen <nttoanbrvt@gmail.com>
2016-11-05 19:58:30 +0700

    no message

--- wp-lite-bitly.php
+++ wp-lite-bitly.php
@@ -7,8 +7,8 @@ Version:        1.0
 Author:         Toan Nguyen
 Author URI:     http://phpface.net
 Text Domain:	wp-lite-bitly
-License:		GPLv2 or later
-License URI:	http://www.gnu.org/licenses/gpl-2.0.html
+License:		GPLv3
+License URI:	http://www.gnu.org/licenses/gpl-3.0.html
 */ 
 if( ! class_exists( 'WP_Lite_Bitly' ) ){
 
